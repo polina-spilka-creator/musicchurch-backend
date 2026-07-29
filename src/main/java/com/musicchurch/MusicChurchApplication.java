ee7957ca21205fe741e2ae743eef501882b21e97
Spilka Polina <spilka88@gmail.com>
2026-07-29 18:53:46 +0300

    Initial project setup

---
+++ src/main/java/com/musicchurch/MusicChurchApplication.java
@@ -0,0 +1,13 @@
+package com.musicchurch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicChurchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicChurchApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/musicchurch/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.musicchurch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Welcome to the Music Church";
+    }
+}
